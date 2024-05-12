ea07b3df6aa501f143e40d27f26e92b88b7cb950
cals <r0922465@student.thomasmore.be>
2024-05-12 18:05:23 +0200

    update 12/05/24

--- FinTrack/NewFolder/Settings.Designer.cs
+++ FinTrack/NewFolder/Settings.Designer.cs
@@ -76,6 +76,7 @@
             button2.TabIndex = 3;
             button2.Text = "arrow back";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // updateprofilebtn
             // 
--- FinTrack/NewFolder/Settings.cs
+++ FinTrack/NewFolder/Settings.cs
@@ -17,30 +17,15 @@ namespace FinTrack.NewFolder
         private void Button3_Click(object sender, EventArgs e)
         {
             // Generate a PDF document with some sample content
-            PdfDocument pdf = new PdfDocument();
-            pdf.Info.Title = "Regulations Document";
-            PdfPage page = pdf.AddPage();
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Verdana", 14, XFontStyle.Bold);
 
-            // Add content to the PDF
-            gfx.DrawString("Company Regulations", font, XBrushes.Black,
-                           new XRect(0, 0, page.Width, page.Height),
-                           XStringFormats.TopCenter);
+        }
 
-            // Add additional text or information here as needed
-            gfx.DrawString("1. Follow safety protocols.\n2. Respect deadlines.\n3. Abide by company values.",
-                           new XFont("Verdana", 12, XFontStyle.Regular),
-                           XBrushes.Black, new XRect(40, 60, page.Width, page.Height),
-                           XStringFormats.TopLeft);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //close the settings formmm !!!: to open overview
 
-            // Save the PDF file to a path (in this case, saving to the project directory)
-            string filePath = "regulations.pdf";
-            pdf.Save(filePath);
+            this.Close();
 
-            // Inform the user of the generated PDF file
-            MessageBox.Show($"The regulations PDF has been created at: {filePath}", "PDF Created",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
